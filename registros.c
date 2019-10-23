daed88f934c8cb9db907b7dfb09d67aef74b1f6f
jucardozo <jucardozo@opc-l-wrk-0003.itbadc.itba.edu.ar>
2019-10-23 13:37:02 -0300

    pa manue

---
+++ registros.c
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: mcorcos
+ *
+ * Created on 21 de octubre de 2019, 14:40
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "registros.h"
+#define Is_portA(i)  (((i)==a)||((i)=A))
+#define Is_portA(i)  (((i)==b)||((i)=B))
+
+static registros_t portd;
+
+void bitSet (char port, int nb){     //p es un puntero a una estructura, bn es el numero de bits//
+    if (Is_portA(i)){
+        
+    }
+    
+    
+}
