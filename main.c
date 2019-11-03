62046545039b87bc0b3412bacf06f11e2dada0c9
enrique <enrique@enrique-VirtualBox>
2019-11-03 02:35:02 -0300

    done

--- main.c
+++ main.c
@@ -17,6 +17,8 @@
 #include <allegro5/allegro_image.h>
 #include "registros.h"
 
+#define HEIGHT 500
+#define WIDTH  800
 #define ltrT(i) ((i)=='t' || (i)=='T')  //cambia todos los bits del puerto por el opuesto
 #define ltrQ(i) ((i)=='q' || (i)=='Q')  //letra q me permite salir del programa
 #define ltrC(i) ((i)=='c' || (i)=='C')  //letra c me permite apagar todos los bits
@@ -24,33 +26,40 @@
 #define numvalido(i)    ((i)>='0' && (i)<='7')
 #define MaskT      0xFF//mascara que sirve para manipular los bits del puerto A
 #define MaskC      0x00
+void create_porta(int bits);
 /*
  * 
  */
 int main() {
     ALLEGRO_DISPLAY  *display=NULL;
     ALLEGRO_BITMAP *imagen=NULL;
+    
     if (!al_init()){        //inicializacion general del allegro
         fprintf (stderr, "error al inicializar el allegro\n");
         return 0;
     }
     if (!al_init_image_addon()){
-        fprintf (stderr, "error al inicializar la imagen\n");
+        fprintf (stderr, "error al inicializar las primitivas\n");
         return 0;
     }
-    imagen=al_load_bitmap("leds.png");        //se carga la imagen, si se inicializa correctamente ocupa memoria dinamica
-    if (!imagen){
-        fprintf(stderr, "error al cargar la imagen");
-        return 0; 
-    }
+
     display=al_create_display(800,500);
+    
     if(!display){
-        al_destroy_bitmap(imagen);      //se destruye la imagen porque ocupa espacio en heap y el programa fallo por otro motivo
-        fprintf(stderr,"error al crear el bitmap");
+        al_shutdown_image_addon();      //se destruye la imagen porque ocupa espacio en heap y el programa fallo por otro motivo
+        fprintf(stderr,"failed to create display");
         return 0;
     }
-    al_draw_bitmap(imagen,0,0,0);
+    al_map_rgb_f(255,255,255);
     al_flip_display();
+    al_map_rgb_f(255,255,255);
+    
+    create_porta(8);
+    
+    
+    
+    
+    
     
     int entrada,loop=1;    //entrada es una variable que me permite almacenar el dato aportado por el usuario, loop, me permite permanecer en el ciclo
     extern registros_t *puertos;
@@ -81,7 +90,21 @@ int main() {
     }while(loop);
     al_destroy_bitmap(imagen);
     al_destroy_display(display);
+    al_shutdown_primitives_addon();
     printf ("bye\n");
+    
+    
     return (EXIT_SUCCESS);
 }
 
+void create_porta(int bits){
+    int i;
+    
+    for(i=1;i<=bits;++i){
+        
+        float value= (WIDTH*i)/(i+1)
+        al_draw_circle( value , HEIGHT /2 , 40.0, al_color_name("black"),5);
+        
+    }
+    al_flip_display();
+}
